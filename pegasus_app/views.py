6bf83c0a24e1f9c505b510b4e91649e8605650bd
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2022-09-20 00:37:58 +0300

    Add form & formset CBV

--- pegasus_app/views.py
+++ pegasus_app/views.py
@@ -8,6 +8,82 @@ from pegasus_app.forms import PhoneNumberCheckConfigForm, ScheduleDayForm, Sched
 from pegasus_app.models import ScheduleDay, PhoneNumberCheckConfig
 
 
+class AssignmentCreateView(generic_view.CreateView):
+    model = Assignment
+    template_name = 'path_to_template_to_assignment_create_form.html'
+    form_class = AssignmentForm
+    object = None
+
+    def get(self, request, *args, **kwargs):
+        """
+        Handles GET requests and instantiates blank versions of the form
+        and its inline formsets.
+        """
+        self.object = None
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        assignment_question_form = AssignmentQuestionFormSet()
+        return self.render_to_response(
+                  self.get_context_data(form=form,
+                                        assignment_question_form=assignment_question_form,
+                                        )
+                                     )
+
+    def post(self, request, *args, **kwargs):
+        """
+        Handles POST requests, instantiating a form instance and its inline
+        formsets with the passed POST variables and then checking them for
+        validity.
+        """
+        self.object = None
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        assignment_question_form = AssignmentQuestionFormSet(self.request.POST)
+        if form.is_valid() and assignment_question_form.is_valid():
+            return self.form_valid(form, assignment_question_form)
+        else:
+            return self.form_invalid(form, assignment_question_form)
+
+    def form_valid(self, form, assignment_question_form):
+        """
+        Called if all forms are valid. Creates Assignment instance along with the
+        associated AssignmentQuestion instances then redirects to success url
+        Args:
+            form: Assignment Form
+            assignment_question_form: Assignment Question Form
+
+        Returns: an HttpResponse to success url
+
+        """
+        self.object = form.save(commit=False)
+        # pre-processing for Assignment instance here...
+        self.object.save()
+
+        # saving AssignmentQuestion Instances
+        assignment_questions = assignment_question_form.save(commit=False)
+        for aq in assignment_questions:
+            #  change the AssignmentQuestion instance values here
+            #  aq.some_field = some_value
+            aq.save()
+
+        return HttpResponseRedirect(self.get_success_url())
+
+    def form_invalid(self, form, assignment_question_form):
+        """
+        Called if a form is invalid. Re-renders the context data with the
+        data-filled forms and errors.
+
+        Args:
+            form: Assignment Form
+            assignment_question_form: Assignment Question Form
+        """
+        return self.render_to_response(
+                 self.get_context_data(form=form,
+                                       assignment_question_form=assignment_question_form
+                                       )
+        )
+
+
 def get_schedule_day_form(phone_config, weekday):
     schedule_day = ScheduleDay.objects.filter(phone_config=phone_config,day=weekday).first()
 
