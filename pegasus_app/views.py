d0d5b6553ece3af456631345501b6107625c5960
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2022-09-20 01:01:49 +0300

    Adjust PhoneNumberCheckConfigCreateView naming

--- pegasus_app/views.py
+++ pegasus_app/views.py
@@ -31,55 +31,55 @@ class PhoneNumberCheckConfigCreateView(CreateView):
 
     def post(self, request, *args, **kwargs):
         """
-        Handles POST requests, instantiating a form instance and its inline
+        Handles POST requests, instantiating a phone_form instance and its inline
         formsets with the passed POST variables and then checking them for
         validity.
         """
         self.object = None
         form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        assignment_question_form = AssignmentQuestionFormSet(self.request.POST)
-        if form.is_valid() and assignment_question_form.is_valid():
-            return self.form_valid(form, assignment_question_form)
+        phone_form = self.get_form(form_class)
+        schedule_day_formset = ScheduleDayFormset(self.request.POST)
+        if phone_form.is_valid() and schedule_day_formset.is_valid():
+            return self.form_valid(phone_form, schedule_day_formset)
         else:
-            return self.form_invalid(form, assignment_question_form)
+            return self.form_invalid(phone_form, schedule_day_formset)
 
-    def form_valid(self, form, assignment_question_form):
+    def form_valid(self, phone_form, schedule_day_formset):
         """
         Called if all forms are valid. Creates Assignment instance along with the
         associated AssignmentQuestion instances then redirects to success url
         Args:
-            form: Assignment Form
-            assignment_question_form: Assignment Question Form
+            phone_form: Assignment Form
+            schedule_day_formset: Assignment Question Form
 
         Returns: an HttpResponse to success url
 
         """
-        self.object = form.save(commit=False)
+        self.object = phone_form.save(commit=False)
         # pre-processing for Assignment instance here...
         self.object.save()
 
         # saving AssignmentQuestion Instances
-        assignment_questions = assignment_question_form.save(commit=False)
-        for aq in assignment_questions:
+        schedule_days = schedule_day_formset.save(commit=False)
+        for schedule_day in schedule_days:
             #  change the AssignmentQuestion instance values here
-            #  aq.some_field = some_value
-            aq.save()
+            #  schedule_day.some_field = some_value
+            schedule_day.save()
 
         return HttpResponseRedirect(self.get_success_url())
 
-    def form_invalid(self, form, assignment_question_form):
+    def form_invalid(self, phone_form, schedule_day_formset):
         """
         Called if a form is invalid. Re-renders the context data with the
         data-filled forms and errors.
 
         Args:
-            form: Assignment Form
-            assignment_question_form: Assignment Question Form
+            phone_form: Phone Form
+            schedule_day_formset: Schedule Day Form
         """
         return self.render_to_response(
-                 self.get_context_data(form=form,
-                                       assignment_question_form=assignment_question_form
+                 self.get_context_data(form=phone_form,
+                                       assignment_question_form=schedule_day_formset
                                        )
         )
 
