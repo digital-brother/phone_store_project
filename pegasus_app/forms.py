f46bb222b092da04ad397f707a6c797b5054ae69
NikGorbunov <gorbunovmya.@gmail.com>
2022-09-20 13:57:11 +0300

    styled phone config staff

--- pegasus_app/forms.py
+++ pegasus_app/forms.py
@@ -5,12 +5,19 @@ from pegasus_app.models import Phone, Schedule
 
 
 class PhoneForm(forms.ModelForm):
+    ima_name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'width: 300px; margin-bottom: 20px;'}))
+    number = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'width: 300px; margin-bottom: 20px;'}))
+    failure_threshold = forms.IntegerField(widget=forms.TextInput(attrs={'type': 'range', 'min': "1", 'max': "10",
+                                                                         'oninput': 'id_output_failure_threshold.value = id_failure_threshold.value'}))
+    test_frequency = forms.IntegerField(
+        widget=forms.TextInput(attrs={'type': 'range', 'min': '10', 'max': '120', 'step': '5',
+                                      'oninput': 'id_output_test_frequency.value = id_test_frequency.value'}))
+
     class Meta:
         model = Phone
         fields = ['ima_name', 'number', 'failure_threshold', 'test_frequency']
-        widgets = {
-            'ima_name': forms.TextInput(attrs={'label': 'Ima name', 'class': 'form-input'}),
-        }
 
 
 class ScheduleForm(forms.ModelForm):
